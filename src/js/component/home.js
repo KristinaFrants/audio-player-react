c5e7a84be15a7755f4d6531eb4f8b15a3852fb87
KristinaFrants <cristina.chiticari@gmail.com>
2020-02-26 15:57:02 +0000

    first commit no css

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,43 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
+// import PropTypes from "prop-types";
 
 //create your first component
-export function Home() {
+export const Home = () => {
+	// initialize the tasks variable to an empty array and hook it to setTasks function
+	const [treck, setTreck] = useState();
+	const [sourse, setSourse] = useState();
+
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/sound/songs")
+			.then(resp => resp.json())
+			.then(data => {
+				setTreck(data), [];
+			});
+	});
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<ul>
+				{!treck
+					? "Loading..."
+					: treck.map((t, index) => {
+							return (
+								<li
+									key={index}
+									onClick={() =>
+										setSourse(
+											"https://assets.breatheco.de/apis/sound/" +
+												t.url
+										)
+									}>
+									{t.name}
+								</li>
+							);
+					  })}
+			</ul>
+			<audio controls autoPlay src={sourse}>
+				<source type="audio/mp3" />
+			</audio>
+		</>
 	);
-}
+};
